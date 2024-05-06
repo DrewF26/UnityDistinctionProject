615b5d2b3547f0a014d05094977054ee4077e4e4
DrewF26 <dfalzarano26@punahou.edu>
2024-05-06 12:34:49 -1000

    changed shield to server, changed server scripts to have health

---
+++ Assets/scripts/ServerCollider.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class ServerCollider : MonoBehaviour
+{
+
+    public int CollisionCount = 0;
+    public bool Detecting = true;
+    PlayerHealth healthScript;
+
+    void Start()
+    {
+        healthScript = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealth>();
+    }
+
+    // starts server collision detection
+    public void StartCollisionDetect(bool detect)
+    {
+        //starts server game
+        Detecting = detect;
+    }
+
+    void OnCollisionEnter(Collision CollisionInfo)
+    {
+        // stops script if Detecting == false
+        if (Detecting == false)
+        {
+            return;
+        }
+        // logs when server collides with "Collision Object"
+        if(CollisionInfo.collider.tag=="Collision Object")
+        {
+            
+            healthScript.UpdateHealth(2);
+            //Collision Object disapears
+        }
+      
+        if (CollisionInfo.collider.tag == "Cylinder")
+        {
+
+            healthScript.UpdateHealth(3);
+        }
+    }
+}
